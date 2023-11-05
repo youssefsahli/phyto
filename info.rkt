c3377aaec6e4bcb05e55bb70d571c0ddb60f3117
sahli.youssef@ymail.com <sahli.youssef@ymail.com>
2023-11-05 14:54:36 +0100

    init.rkt update

--- info.rkt
+++ info.rkt
@@ -3,6 +3,9 @@
 (define name "PhytoCAR")
 (define collection name)
 (define version "0.1")
+(define deps (list (list "base") (list "mischief") (list "gui-easy")))
+(define license 'GPL-3.0-or-later)
+
 (define gracket-launcher-names (list "phyto-car"))
 (define gracket-launcher-libraries (list "src/phyto.rkt"))
 (define pkg-authors (list "Y. Sahli"))
